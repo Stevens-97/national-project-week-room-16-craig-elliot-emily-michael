f7aa6481c5b6d6982b9a469476fa084bf370dc83
Elliott.Stevens <Elliott.Samuel.Stevens@gmail.com>
2022-01-18 13:20:40 +0000

    Fixed test api, added to git ignore
    Removed package-lock from the commit list, added it to gitignore and changed route of the backend

--- app.js
+++ app.js
@@ -17,13 +17,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/users", usersRouter);
+app.use("/bootcamp", usersRouter);
 
 app.use(function (req, res, next) {
    res.status(404).json({
       message: "We couldn't find what you were looking for 😞",
    });
 });
+
+
 
 app.use(function (err, req, res, next) {
    console.error(err.stack);
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,22 @@
-import express from "express";
-const router = express.Router();
+// import express from "express";
+// const router = express.Router();
+// import express from 'express'
+import { Router } from 'express';
+
+// const router = express.Router()
+
+const router = Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
    res.json({ message: "I wish we had some information to give you ☹️" });
 });
 
-router.get("/test", function (req, res, next) {
-   return res.json({ message: "success" });
+router.get('/test', async function (req, res) {
+  res.json({
+      success: true,
+      message: "I am a super API",
+  });
 });
 
 export default router;
