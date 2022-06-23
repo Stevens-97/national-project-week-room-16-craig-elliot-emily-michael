ccea2ea94846f4916a758b83c00a9b2cfb46017a
Stevens-97 <86322472+Stevens-97@users.noreply.github.com>
2022-06-24 00:26:58 +0100

    Added dot env

--- db/connection.js
+++ db/connection.js
@@ -3,15 +3,15 @@ import * as config from "../config.js";
 import "dotenv/config";
 
 const pool = new pg.Pool({
-   user: config.username,
-   host: config.host,
-   database: config.database,
-   password: config.password,
-   port: config.db_port,
-   connectionString: config.connectionString,
-   ssl: { rejectUnauthorized: false },
+  user: config.username,
+  host: config.host,
+  database: config.database,
+  password: config.password,
+  port: config.db_port,
+  // connectionString: config.connectionString,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default function query(text, params) {
-   return pool.query(text, params);
+  return pool.query(text, params);
 }
